b7fc5ab8c383f16dcea59b348a324b74822d9397
Siddhesh Asati <siddheshasati@gmail.com>
2026-06-12 19:27:00 +0530

    fix: serve static files directly in production as fallback in urls.py

--- career_neuron/urls.py
+++ career_neuron/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import include, path, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("", include("portal.urls")),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.BASE_DIR / 'portal' / 'static'}),
 ]
